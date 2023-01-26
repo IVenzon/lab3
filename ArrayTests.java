79d72494c26529bee70639851bb2c00c8cb4bcfd
IVenzon <ijv2543@gmail.com>
2023-01-26 12:58:06 -0800

    fixed ArrayTests

--- ArrayTests.java
+++ ArrayTests.java
@@ -5,8 +5,11 @@ public class ArrayTests {
 	@Test 
 	public void testReverseInPlace() {
     int[] input1 = { 3 };
+    int[] input2 = {5, 4, 3, 2, 1};
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(new int[]{1, 2, 3, 4, 5}, input2);
 	}
 
 
@@ -14,5 +17,11 @@ public class ArrayTests {
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testAvgWithoutLowest() {
+    double[] input3 = {1, 1, 1};
+    assertEquals(0.5, ArrayExamples.averageWithoutLowest(input3), 0);
   }
 }
